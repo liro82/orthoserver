5e490112d0448b5a63fb5343799027adcf607bec
Rolf Linder <rolf.liro.linder@gmail.com>
2018-06-17 14:30:06 +0200

    enable services / dir structure

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -3,6 +3,7 @@
 # config
 app_to_install="git yum-updateonboot httpd mariadb-server mariadb php php-mysql php-gd"
 grp_to_install=("")
+svc_to_enable="httpd mariadb"
 
 # update
 yum -y install epel-release
@@ -20,3 +21,6 @@ yum -y install $app_to_install
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
+
+# enable required services
+systemctl enable $svc_to_enable --now
