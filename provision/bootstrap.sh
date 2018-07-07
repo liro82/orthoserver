7d52024fc09e25218892b5f8e3dbba37dd16c47c
Rolf Linder <rolf.liro.linder@gmail.com>
2018-07-07 13:59:29 +0200

    adding backup/restore functionality

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -24,3 +24,90 @@ firewall-cmd --reload
 
 # enable required services
 systemctl enable $svc_to_enable --now
+
+# setup database
+mysql -u root < /vagrant/provision/db_setup.sql
+
+# install bootup tasks
+cat << EOF > /etc/systemd/system/orthotasks.service
+[Unit]
+Description=Ortho Tasks
+After=mariadb.service vagrant.mount
+Before=shutdown.target
+RequiresMountsFor=/vagrant
+
+[Service]
+ExecStart=/opt/ortholabor/system/restore_data.sh
+ExecStop=/opt/ortholabor/system/backup_data.sh
+Type=oneshot
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable orthotasks.service --now
+
+# install backup / restore scripts
+mkdir -p /opt/ortholabor/system
+cat << EOF > /opt/ortholabor/system/backup_data.sh
+#!/bin/bash
+#
+# backup of data
+#
+
+set -e
+
+# variables
+dbuser=root
+dbname=ortho
+sourcedir=/var/www/html
+destdir=/vagrant/data/backup
+timestring=`date +"%Y%m%d"`
+
+# clean old backups
+find \$destdir -type f -mtime +7 -delete
+
+# backup of database data
+mysqldump -u \$dbuser --databases \$dbname > \$destdir/dbbackup
+gzip \$destdir/dbbackup
+mv -f \$destdir/dbbackup.gz \$destdir/dbbackup-\$timestring.sql.gz
+logger -t ortho "saved db backup to \$destdir/dbbackup-\$timestring.sql.gz"
+
+# backup of application data
+cd \$sourcedir
+rm -f \$destdir/appbackup-\$timestring.tgz
+tar -zcf \$destdir/appbackup-\$timestring.tgz *
+logger -t ortho "saved app backup to \$destdir/appbackup-\$timestring.tgz"
+EOF
+cat << EOF > /opt/ortholabor/system/restore_data.sh
+#!/bin/bash
+#
+# backup of data
+#
+
+set -e
+
+# variables
+dbuser=root
+dbname=ortho
+appdir=/var/www/html
+sourcedir=/vagrant/data/restore
+timestring=`date +"%Y%m%d"`
+
+# check if appbackup to restore
+if ls \$sourcedir/appbackup-* 1> /dev/null 2>&1; then
+  rm -rf \$appdir
+  mkdir -p \$appdir
+  tar -zxf \$sourcedir/appbackup-* -C \$appdir
+  mv -f \$sourcedir/appbackup-* /opt/ortholabor
+  logger -t ortho "restored app backup"
+fi
+
+# check if dbbackup to restore
+if ls \$sourcedir/dbbackup-* 1> /dev/null 2>&1; then
+  zcat \$sourcedir/dbbackup-* | mysql -u root
+  mv -f \$sourcedir/dbbackup-* /opt/ortholabor
+  logger -t ortho "restored db backup"
+fi
+EOF
+chmod +x /opt/ortholabor/system/*.sh
---
+++ provision/db_setup.sql
@@ -0,0 +1,11 @@
+--
+-- Create Database: `ortho`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ortho` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+---
+--- Create user
+---
+
+GRANT ALL ON `ortho`.* TO 'ortho'@'localhost';
